f9474a4cff72dcbf71a833e664094dce6d45d004
Marc Lamberts <43936834+marclamberts@users.noreply.github.com>
2025-09-04 11:53:43 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'soccerdonna_scraper'
+copyright = '2025, Marc Lamberts'
+author = 'Marc Lamberts'
 
 release = '0.1'
 version = '0.1.0'
